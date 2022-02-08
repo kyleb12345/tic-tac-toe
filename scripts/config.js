1bd1949aa2534c15006428926209e280f45d908b
kyle <bartscholar@gmail.com>
2022-02-08 13:33:36 -0600

    first one

---
+++ scripts/config.js
@@ -0,0 +1,32 @@
+function openPlayerConfig() {
+    editedPlayer = +event.target.dataset.playerid; // +'1' => 1 as a number
+    playerConfigOverlayElement.style.display = 'block';
+    backdropElement.style.display = 'block';
+}
+
+function closePlayerConfig() {
+    playerConfigOverlayElement.style.display = 'none';
+    backdropElement.style.display = 'none';
+    formElement.firstElementChild.classList.remove('error');
+    errorsOutputElement.textContent = '';
+    formElement.firstElementChild.lastElementChild.value = ''; // not the best way, but DOM practice to grab elements, could use ID
+}
+
+function savePlayerConfig(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const enteredPlayername = formData.get('playername').trim();
+
+    if (!enteredPlayername) {
+        event.target.firstElementChild.classList.add('error');
+        errorsOutputElement.textContent = 'Please enter a valid name!';
+        return;
+    }
+    const updatedPlayerDataElement = document.getElementById('player-' + editedPlayer + '-data');
+    updatedPlayerDataElement.children[1].textContent = enteredPlayername;
+
+    players[editedPlayer - 1].name = enteredPlayername;
+
+    closePlayerConfig();
+
+}
